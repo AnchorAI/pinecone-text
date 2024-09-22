73ae51db847dbe8c03805bb52cf1e4ea864c7814
Itai <48394109+ibraude@users.noreply.github.com>
2024-09-22 17:47:02 +0300

    Update bm25_tokenizer.py

--- pinecone_text/sparse/bm25_tokenizer.py
+++ pinecone_text/sparse/bm25_tokenizer.py
@@ -36,7 +36,7 @@ class BM25Tokenizer:
         try:
             nltk.data.find("tokenizers/punkt")
         except LookupError:
-            nltk.download("punkt")
+            nltk.download("punkt_tab")
 
         try:
             nltk.data.find("corpora/stopwords")
